7db0e0eef0162771baa3018aa737f8ab15f1b660
Ahmed Mostafa <121709141+AhmedMostafa167@users.noreply.github.com>
2026-06-19 09:22:39 +0000

    created a simple FastAPI app

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def main():
-    print("Hello from mini-rag!")
+from fastapi import FastAPI
+app = FastAPI()
 
-
-if __name__ == "__main__":
-    main()
+@app.get('/welcome')
+def welcome():
+    return {
+        "message": "welcome to my app"
+    }
