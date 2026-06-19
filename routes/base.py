044b1e6306c576976a7cdbe51f4b7070b1bac37b
Ahmed Mostafa <121709141+AhmedMostafa167@users.noreply.github.com>
2026-06-19 12:40:44 +0000

    moved welcome fn into base.py and created __init__py inside routes

---
+++ routes/base.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI, APIRouter
+import os
+
+base_router = APIRouter(
+    prefix="/api/v1",
+    tags=["api_v1"]
+                        )
+
+@base_router.get('/')
+async def welcome():
+    APP_NAME = os.getenv("APP_NAME")
+    APP_VERSION = os.getenv("APP_VERSION")
+    
+    return {
+        "app name": APP_NAME,
+        "app version": APP_VERSION
+    } 
