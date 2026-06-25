237ca58618318d1707dd00047dc37ed717ee6e76
Ahmed Mostafa <121709141+AhmedMostafa167@users.noreply.github.com>
2026-06-25 12:50:38 +0000

    using FastAPI Depends

--- routes/base.py
+++ routes/base.py
@@ -1,15 +1,15 @@
-from fastapi import FastAPI, APIRouter
+from fastapi import FastAPI, APIRouter, Depends
 import os
-
+from helpers.config import get_settings, Settings
 base_router = APIRouter(
     prefix="/api/v1",
     tags=["api_v1"]
                         )
 
 @base_router.get('/')
-async def welcome():
-    APP_NAME = os.getenv("APP_NAME")
-    APP_VERSION = os.getenv("APP_VERSION")
+async def welcome(app_settings: Settings = Depends(get_settings)):
+    APP_NAME = app_settings.APP_NAME
+    APP_VERSION = app_settings.APP_VERSION
     
     return {
         "app name": APP_NAME,
